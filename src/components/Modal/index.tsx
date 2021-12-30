e96fc20868f871b23b843bf600489a43ac42fa74
mariafantuci <mariafantuci@gmail.com>
2021-12-30 17:20:08 -0300

    refatorando o Componente Modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,42 @@
+import { Component, ReactNode } from 'react';
+import ReactModal from 'react-modal';
+
+interface ModalProps {
+  children: ReactNode;
+  setIsOpen: () => void;
+  isOpen: boolean;
+  modalStatus?: boolean;
+}
+
+export function Modal({ children, isOpen, setIsOpen }: ModalProps) {
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={isOpen}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
